8d2be4eecad7198ad88120253bc987821285b211
Kim Valencia <kimvalencia05@gmail.com>
2022-07-20 00:03:59 +0800

    Add nuget packages: Newtonsoft JSON, Mapster, and RestSharp for Web Service Calls

---
+++ IONACodingExercise/WebServices/CatWebService.cs
@@ -0,0 +1,32 @@
+﻿using IONACodingExercise.ExternalDomains.BreedAgg;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace IONACodingExercise.WebServices
+{
+    public class CatWebService
+    {
+        RestClient client;
+        readonly string API_KEY= "bb16bac4-8615-4273-9f91-01f713be074a";
+        public CatWebService()
+        {
+            client = new RestClient("https://api.thecatapi.com");
+            client.AddDefaultHeader("x-api-key", API_KEY);
+        }
+
+        public List<CatBreedDTO> GetCatBreeds(int limit, int page)
+        {
+            RestRequest request = new RestRequest("v1/breeds");
+            request.AddParameter("limit", limit);
+            request.AddParameter("page", page);
+
+            var result = client.ExecuteGet(request);
+
+            if (result.IsSuccessful)
+            {
+                return JsonConvert.DeserializeObject<List<CatBreedDTO>>(result.Content);
+            }
+            return null;
+        }
+    }
+}
---
+++ IONACodingExercise/WebServices/DogWebService.cs
@@ -0,0 +1,32 @@
+﻿using IONACodingExercise.ExternalDomains.BreedAgg;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace IONACodingExercise.WebServices
+{
+    public class DogWebService
+    {
+        RestClient client;
+        readonly string API_KEY = "d60209ef-c9da-42a2-9146-794fa3861299";
+        public DogWebService()
+        {
+            client = new RestClient("https://api.thedogapi.com");
+            client.AddDefaultHeader("x-api-key", API_KEY);
+        }
+
+        public List<DogBreedDTO> GetDogBreeds(int limit, int page)
+        {
+            RestRequest request = new RestRequest("v1/breeds");
+            request.AddParameter("limit", limit);
+            request.AddParameter("page", page);
+
+            var result = client.ExecuteGet(request);
+
+            if (result.IsSuccessful)
+            {
+                return JsonConvert.DeserializeObject<List<DogBreedDTO>>(result.Content);
+            }
+            return null;
+        }
+    }
+}
