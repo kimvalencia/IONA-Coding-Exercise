b6c7212d14750a3ae5f5a59c9c35665cd4787866
Kim Valencia <kimvalencia05@gmail.com>
2022-07-20 23:29:55 +0800

    Added Unit Testing to test Sorting Algorithms and different Scenarios

---
+++ IONACodingExercise.Test/BreedImageSorterTest.cs
@@ -0,0 +1,32 @@
+﻿using IONACodingExercise.BLL;
+using IONACodingExercise.Test.DataGenerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IONACodingExercise.Test
+{
+    [TestClass]
+    public class BreedImageSorterTest
+    {
+        [TestMethod]
+        public void CombineDogAndCatImages_Simple_1stElementCat()
+        {
+            var CatImages = new BreedImageGenerator().GetCatBreedImages();
+            var DogImages = new BreedImageGenerator().GetDogBreedImages();
+            var result = new BreedSorter().CombineDogAndCatBreedImages(CatImages, DogImages, 10, 1);
+            Assert.AreEqual("A Cat", result[0].id);
+        }
+
+        [TestMethod]
+        public void CombineDogAndCatImages_Simple_Count10()
+        {
+            var CatImages = new BreedImageGenerator().GetCatBreedImages();
+            var DogImages = new BreedImageGenerator().GetDogBreedImages();
+            var result = new BreedSorter().CombineDogAndCatBreedImages(CatImages, DogImages, 10, 1);
+            Assert.AreEqual(10, result.Count);
+        }
+    }
+}
--- IONACodingExercise.Test/BreedServiceTest.cs
+++ IONACodingExercise.Test/BreedServiceTest.cs
@@ -1,6 +1,10 @@
-using IONACodingExercise.BLL;
-using IONACodingExercise.Domain.BreedAgg;
-using IONACodingExercise.Test.DataGenerators;
+﻿using IONACodingExercise.Services;
+using IONACodingExercise.Test.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace IONACodingExercise.Test
 {
@@ -8,63 +12,31 @@ namespace IONACodingExercise.Test
     public class BreedServiceTest
     {
         [TestMethod]
-        public void CombineDogAndCatBreeds_Simple_1stElementCat()
+        public void GetCombinedBreeds_Page1Limit10_Return10()
         {
-            var dogs = new DogDataGenerator().GetSimpleDogList();
-            var cats = new CatDataGenerator().GetSimpleCatList();
-
-            var result = new BreedSorter().CombineDogAndCatBreeds(cats, dogs, 10, 1);
-
-            Assert.AreEqual("A Cat", result[0].name);
-        }
-
-        [TestMethod]
-        public void CombineDogAndCatBreeds_Simple_10Result()
-        {
-            var dogs = new DogDataGenerator().GetSimpleDogList();
-            var cats = new CatDataGenerator().GetSimpleCatList();
-
-            var result = new BreedSorter().CombineDogAndCatBreeds(cats, dogs, 10, 1);
+           var service = new BreedService(new MockCatWebService(), new MockDogWebService());
+            var result = service.GetCombinedBreeds(10, 1);
 
             Assert.AreEqual(10, result.Count);
+
         }
 
         [TestMethod]
-        public void CombineDogAndCatBreeds_Simple_5Dog5Cats()
+        public void GetCombinedBreeds_Page1Limit10_1stElementCat()
         {
-            var dogs = new DogDataGenerator().GetSimpleDogList();
-            var cats = new CatDataGenerator().GetSimpleCatList();
+            var service = new BreedService(new MockCatWebService(), new MockDogWebService());
+            var result = service.GetCombinedBreeds(10, 1);
 
-            var result = new BreedSorter().CombineDogAndCatBreeds(cats, dogs, 10, 1);
-            
-            //Dog Count
-            Assert.AreEqual(5, result.Where(q=>q.name.ToLower().Contains("dog")).ToList().Count);
-            //Cat Count
-            Assert.AreEqual(5, result.Where(q=>q.name.ToLower().Contains("cat")).ToList().Count);
+            Assert.AreEqual("1 Cat", result[0].name);
         }
 
         [TestMethod]
-        public void CombineDogAndCatBreeds_EmptyDog_10Cats()
+        public void GetCombinedBreeds_Page2Limit10_1stElement19Cat()
         {
-            var dogs = new DogDataGenerator().GetEmptyDogList();
-            var cats = new CatDataGenerator().GetSimpleCatList();
+            var service = new BreedService(new MockCatWebService(), new MockDogWebService());
+            var result = service.GetCombinedBreeds(10, 2);
 
-            var result = new BreedSorter().CombineDogAndCatBreeds(cats, dogs, 10, 1);
-
-            //Cat Count
-            Assert.AreEqual(10, result.Where(q => q.name.ToLower().Contains("cat")).ToList().Count);
-        }
-
-        [TestMethod]
-        public void CombineDogAndCatBreeds_EmptyCat_10Dogs()
-        {
-            var dogs = new DogDataGenerator().GetSimpleDogList();
-            var cats = new CatDataGenerator().GetEmptyCatList();
-
-            var result = new BreedSorter().CombineDogAndCatBreeds(cats, dogs, 10, 1);
-
-            //Dog Count
-            Assert.AreEqual(10, result.Where(q => q.name.ToLower().Contains("dog")).ToList().Count);
+            Assert.AreEqual("19 Cat", result[0].name);
         }
     }
 }
